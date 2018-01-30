97a0a3b73024719292c8e24675db888cab4acda4
Lisa Gee <zhusufeng@gmail.com>
2018-01-29 17:32:16 -0800

    Send text from input in Form.jsx up to App.jsx

--- client/Form.jsx
+++ client/Form.jsx
@@ -2,14 +2,16 @@ class Form extends React.Component {
   render() {
     return (
       <div>
-        <form 
+        <form
+          id="user-form" 
           onSubmit={e => {
             e.preventDefault();
-            this.props.handleInput('Message from form');
+            let userInput = document.getElementById('user-input').value;
+            this.props.handleInput(userInput);
           }}
         >
-          <input type="text" placeholder="Text here" />
-          <input type="submit" value="Submit" />
+          <input type="text" id="user-input" placeholder="Text here" />
+          <input type="submit" id="user-submit" value="Submit" />
         </form>
       </div>
     );
