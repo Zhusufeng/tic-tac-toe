12514591bd83e2fcd180dfb799d6950226e1ff38
Lisa Gee <zhusufeng@gmail.com>
2018-01-29 18:06:16 -0800

    Rename square's id's on Board.jsx

--- client/Board.jsx
+++ client/Board.jsx
@@ -3,15 +3,15 @@ class Board extends React.Component {
   render() {
     return (
       <div id="board">
-        <div id="1">1</div>
-        <div id="2">2</div>
-        <div id="3">3</div>
-        <div id="4">4</div>
-        <div id="5">5</div>
-        <div id="6">6</div>
-        <div id="7">7</div>
-        <div id="8">8</div>
-        <div id="9">9</div>
+        <div className="square" id="box-1">1</div>
+        <div className="square" id="box-2">2</div>
+        <div className="square" id="box-3">3</div>
+        <div className="square" id="box-4">4</div>
+        <div className="square" id="box-5">5</div>
+        <div className="square" id="box-6">6</div>
+        <div className="square" id="box-7">7</div>
+        <div className="square" id="box-8">8</div>
+        <div className="square" id="box-9">9</div>
       </div>
     );
   }
