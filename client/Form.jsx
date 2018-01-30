be8e0401e99a10f0a77f1c938e67c939427a0508
Lisa Gee <zhusufeng@gmail.com>
2018-01-29 17:28:22 -0800

    Create a simple form & test submit/this.props.handleInput in Form.jsx

--- client/Form.jsx
+++ client/Form.jsx
@@ -1,7 +1,17 @@
 class Form extends React.Component {
   render() {
-    return(
-      <p>Form here</p>
+    return (
+      <div>
+        <form 
+          onSubmit={e => {
+            e.preventDefault();
+            this.props.handleInput('Message from form');
+          }}
+        >
+          <input type="text" placeholder="Text here" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
