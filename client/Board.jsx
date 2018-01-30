1795dc29f6568fb7e2ddea5e267cafdf258b7a47
Lisa Gee <zhusufeng@gmail.com>
2018-01-29 17:55:05 -0800

    Create skeleton of a simple Board in Board.jsx

--- client/Board.jsx
+++ client/Board.jsx
@@ -1,8 +1,17 @@
 class Board extends React.Component {
+
   render() {
     return (
-      <div>
-        I am board.
+      <div id="board">
+        <div id="1">1</div>
+        <div id="2">2</div>
+        <div id="3">3</div>
+        <div id="4">4</div>
+        <div id="5">5</div>
+        <div id="6">6</div>
+        <div id="7">7</div>
+        <div id="8">8</div>
+        <div id="9">9</div>
       </div>
     );
   }
