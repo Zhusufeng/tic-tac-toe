1a7c5edd88afb45064f85a576ae249184142abb0
Lisa Gee <zhusufeng@gmail.com>
2018-01-29 17:18:01 -0800

    Build out render of App with title, descrip & instruc

--- client/App.jsx
+++ client/App.jsx
@@ -8,7 +8,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h1 id="title">Tic Tac Toe</h1>
+        <div id="description">
+          <p>Description of game here.
+          </p>
+        </div>
+        <div id="instructions">
+          <p>Instructions here.
+          </p>
+        </div>
+      </div>
     );
   }
 }
