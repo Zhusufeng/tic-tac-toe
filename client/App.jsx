c68191d1bf85da6629fa0c5dc5a3f94667dcd7cb
Lisa Gee <zhusufeng@gmail.com>
2018-01-29 17:13:53 -0800

    Build a simple skeleton of App.jsx

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
